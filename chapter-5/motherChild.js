7e91ad73a8f07e80d45f6b606ff3bba7fd4af5e8
Tristan Rubadeau <tristan@rubadeau.com>
2015-04-23 15:15:46 -0600

    motherChild

---
+++ chapter-5/motherChild.js
@@ -0,0 +1,13 @@
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+// Your code here.
+
+// → 31.2
